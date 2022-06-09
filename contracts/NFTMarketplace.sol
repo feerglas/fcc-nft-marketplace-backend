6e83ba790b927d4dc688d2950ad724a4daee91a7
Yves Tscherry <info@feerglas.com>
2022-06-09 10:04:34 +0200

    implement buy functionality

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.7;
 
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 
 ////////////////////////////////////
 // Errors                         //
@@ -9,9 +10,15 @@ import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 error NFTMarketplace__PriceMustBeAboveZero();
 error NFTMarketplace__NotApprovedForMarketplace();
 error NFTMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NFTMarketplace__NotListed(address nftAddress, uint256 tokenId);
 error NFTMarketplace__NotOwner();
+error NFTMarketplace__PriceNotMet(
+  address nftAddress,
+  uint256 tokenId,
+  uint256 price
+);
 
-contract NFTMarketplace {
+contract NFTMarketplace is ReentrancyGuard {
   struct Listing {
     uint256 price;
     address seller;
@@ -27,20 +34,34 @@ contract NFTMarketplace {
     uint256 price
   );
 
+  event ItemBought(
+    address indexed buyer,
+    address indexed nftAddress,
+    uint256 indexed tokenId,
+    uint256 price
+  );
+
   // NFT contract address -> NFT token ID -> Listing
   mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+  // seller address -> amount earned
+  mapping(address => uint256) private s_proceeds;
 
   ////////////////////////////////////
   // Modifiers                      //
   ////////////////////////////////////
-  modifier notListed(
-    address nftAddress,
-    uint256 tokenId,
-    address owner
-  ) {
+  modifier notListed(address nftAddress, uint256 tokenId) {
     Listing memory listing = s_listings[nftAddress][tokenId];
     if (listing.price > 0) {
       revert NFTMarketplace__AlreadyListed(nftAddress, tokenId);
+    }
+    _;
+  }
+
+  modifier isListed(address nftAddress, uint256 tokenId) {
+    Listing memory listing = s_listings[nftAddress][tokenId];
+    if (listing.price <= 0) {
+      revert NFTMarketplace__NotListed(nftAddress, tokenId);
     }
     _;
   }
@@ -75,7 +96,7 @@ contract NFTMarketplace {
     uint256 price
   )
     external
-    notListed(nftAddress, tokenId, msg.sender)
+    notListed(nftAddress, tokenId)
     isOwner(nftAddress, tokenId, msg.sender)
   {
     if (price <= 0) {
@@ -95,6 +116,37 @@ contract NFTMarketplace {
 
     s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
     emit ItemListed(msg.sender, nftAddress, tokenId, price);
+  }
+
+  function buyItem(address nftAddress, uint256 tokenId)
+    external
+    payable
+    nonReentrant
+    isListed(nftAddress, tokenId)
+  {
+    Listing memory listedItem = s_listings[nftAddress][tokenId];
+    if (msg.value < listedItem.price) {
+      revert NFTMarketplace__PriceNotMet(nftAddress, tokenId, listedItem.price);
+    }
+
+    // we don't want to send the money to the seller (push)
+    // instead, we want them to actively withdraw it (pull)
+    // PULL OVER PUSH
+    s_proceeds[listedItem.seller] = s_proceeds[listedItem.seller] + msg.value;
+    delete (s_listings[nftAddress][tokenId]);
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // REENTRANCY ATTACK: we call the transfer methd after all the state
+    // changes. Doing it before the state changes would be a huge security risk
+    //
+    // see contracts/playground/ReentrantVulnerable.sol
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    IERC721(nftAddress).safeTransferFrom(
+      listedItem.seller,
+      msg.sender,
+      tokenId
+    );
+    emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
   }
 }
 
