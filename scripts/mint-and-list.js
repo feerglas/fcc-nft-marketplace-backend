414ec6bd98e9df7c8f686a5ecb3c0762f75b294a
Yves Tscherry <info@feerglas.com>
2022-06-10 09:20:40 +0200

    move blocks after mint and list

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,4 +1,5 @@
-const { ethers } = require('hardhat');
+const { ethers, network } = require('hardhat');
+const { moveBlocks } = require('../utils/move-block');
 
 const PRICE = ethers.utils.parseEther('0.1');
 
@@ -23,6 +24,10 @@ async function mintAndList() {
   await tx.wait(1);
 
   console.log('--->>> Listed Nft...');
+
+  if (network.config.chainId == '31337') {
+    await moveBlocks(2, (sleepAmount = 1000));
+  }
 }
 
 mintAndList()
