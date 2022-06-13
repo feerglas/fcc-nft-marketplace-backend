07a0c0f89d432263e89188b7bf9f35478c8032b5
Yves Tscherry <info@feerglas.com>
2022-06-13 09:29:14 +0200

    reduce plock forwarding to 1 block

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -26,7 +26,7 @@ async function mintAndList() {
   console.log('--->>> Listed Nft...');
 
   if (network.config.chainId == '31337') {
-    await moveBlocks(2, (sleepAmount = 1000));
+    await moveBlocks(1, (sleepAmount = 1000));
   }
 }
 
