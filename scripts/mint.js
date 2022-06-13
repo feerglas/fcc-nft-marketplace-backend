aa8adbabcf689861a49cd6c50fbbae43c50a41dd
Yves Tscherry <info@feerglas.com>
2022-06-13 11:03:33 +0200

    remove unneeded stuff

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,16 +1,15 @@
 const { ethers, network } = require('hardhat');
 const { moveBlocks } = require('../utils/move-block');
 
-const PRICE = ethers.utils.parseEther('0.1');
-
 async function mint() {
-  const nftMarketplace = await ethers.getContract('NFTMarketplace');
   const basicNft = await ethers.getContract('BasicNft');
 
   console.log('--->>> Minting Nft...');
 
   const mintTx = await basicNft.mintNft();
   const mintTxReceipt = await mintTx.wait(1);
+  const tokenId = mintTxReceipt.events[0].args.tokenId;
+  console.log(`Got tokenId: ${tokenId}`);
 
   if (network.config.chainId == '31337') {
     await moveBlocks(1, (sleepAmount = 1000));
